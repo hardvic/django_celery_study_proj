c08572a4a405ca408e4082eca1c1f55bc72c5ea0
hardvic <hardvic@lookout.com>
2016-08-26 14:43:28 +0800

    在 view 中添加对应的 worker.

--- demoapp/views.py
+++ demoapp/views.py
@@ -1,3 +1,16 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from demoapp.tasks import add
 
 # Create your views here.
+
+
+def index(request):
+    p1 = request.GET.get('p1')
+    p2 = request.GET.get('p2')
+    add.delay(p1, p2)
+    return HttpResponse("hi, you're in djcelery_pro index")
